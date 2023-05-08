b80137f11272677412acb5112875460163f261f4
Leumas2006 <helland.samuel@gmail.com>
2023-05-08 08:29:52 +0200

    Portofolje

---
+++ MinPortofolgje/Javascript/javascript_basics.js
@@ -0,0 +1,18 @@
+var text = "Velkommen";
+
+let na = prompt("your name", "Name");
+
+console.log(text, na);
+
+alert(na);
+
+var x = 10;
+var y = 35;
+
+var sum = x + y;
+
+document.getElementById("Header").style.color = "red";
+document.getElementById("1").style.color = "blue";
+document.getElementById("1").style.fontSize = "24px";
+document.getElementById("2").style.color = "lightblue";
+document.getElementById("body").style.backgroundColor = "black";
